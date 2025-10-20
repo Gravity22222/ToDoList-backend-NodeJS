5e130746a13d303a729a88ed834011cc02db3002
Gravity22222 <89166719+Gravity22222@users.noreply.github.com>
2025-10-20 16:46:42 -0300

    fix: Read server port from env variable or default to 3333

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
 import app from './app.js';
 
-app.listen(YOUR_PORT); // example: 3333
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
