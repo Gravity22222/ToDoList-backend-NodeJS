ccfc8467b5bf98c06d7f68c7cf2766ae453bdaeb
Gravity22222 <89166719+Gravity22222@users.noreply.github.com>
2025-10-21 11:16:59 -0300

    refactor: Clean up unused authConfig variable in middleware

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import * as authConfig from '../../config/auth.cjs';
-const resolvedAuthConfig = authConfigCjs.default || authConfig;
+const resolvedAuthConfig = authConfig.default || authConfig;
 
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
